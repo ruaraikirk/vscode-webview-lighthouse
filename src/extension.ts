825964acac9bb1d7b4bc789bd8337ba6b24cbbd6
Eoin O'Neill <eoin.o-neill@sap.com>
2019-06-25 15:25:46 +0100

    lighthouse

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,8 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 const opts = {
-    chromeFlags: ['--show-paint-rects']
+	chromeFlags: ['--show-paint-rects', '--disable-gpu','--no-sandbox', '--disable-web-security',
+	'--disable-dev-shm-usage', '--headless']
   };
 
 export function activate(context: vscode.ExtensionContext) {
@@ -27,12 +28,14 @@ export function activate(context: vscode.ExtensionContext) {
 			message => {
 				switch (message.command) {
 				case "GenerateLemon":
-					vscode.window.showInformationMessage("Message recieved from webview!");
-						// go off and generate lemon stats
-						launchChromeAndRunLighthouse('http://www.sap.com', opts).then(function(results) {
-							panel.webview.postMessage({ results: 'DONE!!' });
-						})
-					return;
+					vscode.window.showInformationMessage("Starting the Run for Lighthouse");
+					// go off and generate lemon stats
+					launchChromeAndRunLighthouse('http://www.sap.com', opts)
+							.then((res) => {
+								panel.webview.postMessage({ results: JSON.stringify(res) })
+								vscode.window.showInformationMessage("Lighthouse Run Complete");
+								return;
+							});
 				}
 			},
 			undefined,
@@ -46,10 +49,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 async function launchChromeAndRunLighthouse(url: string, opts: any, config: any = null) {
     const chrome = await chromeLauncher.launch({chromeFlags: opts.chromeFlags});
-    opts.port = chrome.port;
+	opts.port = chrome.port;
+	opts.emulatedFormFactor = 'none';
+	opts.throttlingMethod = 'provided';
+	opts.disableStorageReset = 'true';
+	opts.output = 'json';
     const results = await lighthouse(url, opts, config);
-    await chrome.kill();
-    console.log(results);
+	await chrome.kill();
+	return results;
 }
 
 function getWebviewContent() {
@@ -61,8 +68,8 @@ function getWebviewContent() {
 					<title>Cat Coding</title>
 				</head>
 				<body>
-					<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-					<button type=button onclick="sendMessage()">Click me!</button>
+					<div id="Button">Click for lighthouse</div>
+					<button type=button onclick="sendMessage()">Click To Start Lighthouse run</button>
 					<script>
 						const vscode = acquireVsCodeApi();
 						function sendMessage(){
@@ -70,9 +77,11 @@ function getWebviewContent() {
 						}
 
 						window.addEventListener('message', event => {
+							document.getElementById('container').innerHTML = "<p>See Results below</p>";
 							document.getElementById('results').innerHTML = event.data.results;
 						});						
 					</script>
+					<div id="container"></div>
 					<div id="results">Results go here</div>
 				</body>
 			</html>`;
